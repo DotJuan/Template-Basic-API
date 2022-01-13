72a76f22e9165e43a37d63329616625b0aebebdd
DotJuan <juan.manuel.masync@icloud.com>
2022-01-13 16:14:07 -0500

    /products created

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express_server = require('express');
 const app = express_server();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/prod', (req, res) => {
   res.send(`Hi!`);
 });
 
-app.get('/getRuta', (req, res) => {
-  res.json({
-    data: {
-      name: 'Juan',
-      product: 'Ventas',
-      price: 2300,
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Chorizo San Carlitano',
+      price: 15000,
+      units: 6,
     },
-  });
+    { name: 'Chorizo Mini San Carlitano', price: 15000, units: 12 },
+    { name: 'Carne Res San Carlitano', price: 15000, units: 1 },
+  ]);
 });
 
 app.listen(port, () => {
